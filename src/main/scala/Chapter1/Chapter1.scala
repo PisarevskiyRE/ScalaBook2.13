ce630da6d80d536a861732eb64f94965ed320539
Roy <daimon--@live.ru>
2022-12-08 12:06:52 +0300

    test define

--- src/main/scala/Chapter1/Chapter1.scala
+++ src/main/scala/Chapter1/Chapter1.scala
@@ -5,6 +5,17 @@ object Chapter1 extends App {
   def printMsg(msg:String): Unit ={
     println(msg)
   }
+  def max(x:Int, y:Int): Int ={
+    if (x>y) x
+    y
+  }
 
   printMsg("Test")
+
+  println(max(5,1).toString)
+
 }
+
+
+
+
--- src/main/scala/Chapter1/WorkSheet1.sc
+++ src/main/scala/Chapter1/WorkSheet1.sc
@@ -1,2 +1,9 @@
 // тест
 println("Test")
+
+def max(x:Int, y:Int): Int ={
+  if (x>y) x
+  y
+}
+
+max(1,5)
