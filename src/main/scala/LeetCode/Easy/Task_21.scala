66f528029f37ce43c9493be5916120147a5d3864
PisarevskiyRE <daimon--@live.ru>
2024-01-24 00:06:13 +0300

    leetcode

--- src/main/scala/LeetCode/Easy/Task_21.scala
+++ src/main/scala/LeetCode/Easy/Task_21.scala
@@ -9,11 +9,68 @@ object Task_21 extends App{
 
   def mergeTwoLists(list1: ListNode, list2: ListNode): ListNode = {
 
-    list1
+    def loop(a: ListNode, b: ListNode, ab: ListNode = new ListNode()): ListNode =
+      (a, b) match {
+        case (null, null) => ab
+        case (i, null) => ab.next = i; ab
+        case (null, j) => ab.next = j; ab
+        case (i, j) =>
+          if (i.x < j.x) {
+            ab.next = i
+            loop(i.next, j, ab.next)
+          } else {
+
+            ab.next = j
+            loop(i, j.next, ab.next)
+          }
+      }
+
+
+    if (list1 == null) list2
+    else if (list2 == null) list1
+    else {
+      val mergedList = new ListNode()
+      loop(list1, list2, mergedList)
+      mergedList.next
+    }
   }
 
 
-  println(
-    mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4, null))), new ListNode(1, new ListNode(3, new ListNode(4, null))))
-  )
+//  def mergeTwoLists2(list1: ListNode, list2: ListNode): ListNode = {
+//    def merge(a: ListNode, b: ListNode, merged: ListNode): ListNode = {
+//      if (a == null) {
+//        merged.next = b
+//        merged
+//      } else if (b == null) {
+//        merged.next = a
+//        merged
+//      } else {
+//        if (a.x < b.x) {
+//          merged.next = a
+//          merge(a.next, b, merged.next)
+//        } else {
+//          merged.next = b
+//          merge(a, b.next, merged.next)
+//        }
+//      }
+//    }
+//
+//    if (list1 == null) list2
+//    else if (list2 == null) list1
+//    else {
+//      val mergedList = new ListNode()
+//      merge(list1, list2, mergedList)
+//      mergedList.next
+//    }
+//  }
+
+
+  var t =  mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4, null))), new ListNode(1, new ListNode(3, new ListNode(4, null))))
+
+  while(t != null) {
+    println(t.x)
+    t = t.next
+  }
+
+
 }
--- src/main/scala/LeetCode/Easy/untitled.sc
+++ src/main/scala/LeetCode/Easy/untitled.sc
@@ -1,14 +1,40 @@
-import scala.collection.mutable.Stack
+class ListNode(_x: Int = 0, _next: ListNode = null) {
+  var next: ListNode = _next
+  var x: Int = _x
+}
 
-val test: Stack[Int] = Stack(1,2,3)
+def mergeTwoLists(list1: ListNode, list2: ListNode): ListNode = {
+  def merge(a: ListNode, b: ListNode, merged: ListNode): ListNode = (a, b) match {
+    case (null, null) => merged
+    case (i, null) =>
+      merged.next = i
+      merged
+    case (null, j) =>
+      merged.next = j
+      merged
+    case (i, j) =>
+      if (i.x < j.x) {
+        merged.next = i
+        merge(i.next, j, merged.next)
+      } else {
+        merged.next = j
+        merge(i, j.next, merged.next)
+      }
+  }
+
+  if (list1 == null) list2
+  else if (list2 == null) list1
+  else {
+    val mergedList = new ListNode()
+    merge(list1, list2, mergedList)
+    mergedList.next
+  }
+}
 
 
-test
+var t =   mergeTwoLists(new ListNode(1, new ListNode(2, new ListNode(4, null))), new ListNode(1, new ListNode(3, new ListNode(4, null))))
 
-test.append(4)
-
-test.pop()
-test.pop()
-test.pop()
-
-test
+while(t != null) {
+  println(t.x)
+  t = t.next
+}
