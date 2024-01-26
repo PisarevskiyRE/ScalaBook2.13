debb641b5e242cfca3f987f6ef2aa04f8ef9836d
PisarevskiyRE <daimon--@live.ru>
2024-01-27 01:07:48 +0300

    leetcode

--- src/main/scala/LeetCode/Easy/Task_30.scala
+++ src/main/scala/LeetCode/Easy/Task_30.scala
@@ -6,59 +6,20 @@ object Task_30 extends App {
 
   def findSubstring(s: String, words: Array[String]): List[Int] = {
 
-    var res = List[Int]()
+    val n = s.length
+    val m = words.length
+    val k = words(0).length
 
-    val wordMap = mutable.Map[String, Int]()
-    val len: Int = words(0).length
-    val n: Int = s.length
-    val m: Int = words.length
+    if (n < m * k) return Nil
 
-
-    words.foreach( x => {
-      wordMap.put(x, wordMap.getOrElse(x,0) + 1 )
-    })
+    val count = words.groupBy(identity).mapValues(_.length)
 
 
-    (0 until len).foreach( i => {
-      var cnt = 0
-      val copy = wordMap
-
-      (i to (n - len)).by(len).foreach( j => {
-        val cur = s.substring(j,j+len)
-
-
-
-
-        copy.update(cur, wordMap.getOrElse(cur,0) - 1 )
-
-        println(copy)
-
-        if (copy(cur) >= 0) cnt += 1
-
-
-        val pop_start = j - (m * len)
-
-        if (pop_start >= 0) {
-          val pop_word = s.substring(pop_start, j + len)
-
-          copy.put(pop_word, copy.getOrElse(pop_word,0) + 1 )
-
-          if (copy(pop_word) > 0) cnt -= 1
-
-        }
-
-        if (cnt == m) {
-          res.appended(pop_start + len)
-        }
-
-      })
-    })
-
-
-    res
+    println(count)
+    List()
   }
 
 
-  println(findSubstring("barfoofoobarthefoobarman", Array("bar","foo","the")))
+  findSubstring("barfoothefoobarman", Array("foo","bar")).foreach(println)
 
 }
--- src/main/scala/LeetCode/Easy/untitled.sc
+++ src/main/scala/LeetCode/Easy/untitled.sc
@@ -1,8 +1,32 @@
-import scala.collection.mutable
+def findSubstring(s: String, words: Array[String]): List[Int] = {
+  val n = s.length
+  val m = words.length
+  val k = words.headOption.map(_.length).getOrElse(0)
+  if (n < m * k) return Nil
+  val count = words.groupBy(identity).mapValues(_.length)
+  var res = List[Int]()
+  for (i <- 0 until k) {
+    var left = i
+    var right = i
+    var map = Map[String, Int]()
+    while (right + k <= n) {
+      val word = s.substring(right, right + k)
+      right += k
+      if (count.contains(word)) {
+        map += (word -> (map.getOrElse(word, 0) + 1))
+        while (map(word) > count(word)) {
+          val leftWord = s.substring(left, left + k)
+          map += (leftWord -> (map(leftWord) - 1))
+          left += k
+        }
+        if (right - left == m * k) res ::= left
+      } else {
+        map = Map[String, Int]()
+        left = right
+      }
+    }
+  }
+  res.reverse
+}
 
-val wordMap = mutable.Map[String, Int]()
-
-
-wordMap += ("a"-> 1 )
-
-wordMap -=  "a"
+findSubstring("abc", Array("a","b","c")).foreach(println)
